02f30a3b6f46f030667e5ecc434e422c5297216c
Bernhard König <github@diro.ch>
2024-12-26 08:15:58 +0100

    ed

--- src/infinite_loop.jl
+++ src/infinite_loop.jl
@@ -33,7 +33,7 @@ catch e
     @warn("Failed to access InfluxDB. See above!")
 end
 
-THRESHOLD_IN_TB = 18.5 #we are currently using the SSD volume (space is limited!)
+THRESHOLD_IN_TB = 30.5 #we are currently using the SSD volume (space is limited!)
 
 nsecsleep = 10*60
 while true
