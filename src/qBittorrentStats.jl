af2262838c5ae06205ff88ffa8fdc9f300db72c1
Bernhard König <github@diro.ch>
2025-01-01 22:37:12 +0100

    a

--- src/qBittorrentStats.jl
+++ src/qBittorrentStats.jl
@@ -19,10 +19,19 @@ include("disk_space.jl")
 
 function __init__()
     py"""import shutil
-    def pydiskspace(pt):
+    import os
+
+    def DOES_NOT_WORK_pydiskspace(pt):
         print(pt)
         return shutil.disk_usage(pt)
-    """    
+
+    def get_fs_freespace(pathname):
+        "Get the free space of the filesystem containing pathname"
+        stat= os.statvfs(pathname)
+        # use f_bfree for superuser, or f_bavail if filesystem
+        # has reserved space for superuser
+        return stat.f_bfree*stat.f_bsize
+    """
 return nothing 
 end
 
