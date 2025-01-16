b374aa1ba7fa18967510e568f5f564a8366c86b7
Bernhard König <github@diro.ch>
2025-01-16 13:01:11 +0100

    ed

--- src/infinite_loop.jl
+++ src/infinite_loop.jl
@@ -2,25 +2,32 @@ using Pkg;
 Pkg.activate("."); Pkg.instantiate()
 using Dates; using DataFrames; using StatsBase; using qBittorrentStats;import InfluxDBClient; import JSON3
 
-configfiles = [raw"\\ds\data\configs\qbittorrentstats\config.json","/volume1/data/configs/qbittorrentstats/config.json","/cfgfolder/qbittorrentstats/config.json"]
-@assert any(isfiletry.(configfiles))
-cfgs,configfile,configfilehash,influxdbsettings = get_config(configfiles)
-#cfgs,configfile,configfilehash,influxdbsettings = rescan_config(cfgs,configfile,configfilehash,influxdbsettings)
-#i=2
- 
-nsecsleep = 10*60
-while true
 
-    for i=1:size(cfgs,1)
-        try
-            cfg = cfgs[i]
-            monitor_instance(cfg)
-            cfgs,configfile,configfilehash,influxdbsettings = rescan_config(cfgs,configfile,configfilehash,influxdbsettings);
-        catch er
-            @show er
-        end
-    end
+function main_fn()
+    configfiles = [raw"\\ds\data\configs\qbittorrentstats\config.json","/volume1/data/configs/qbittorrentstats/config.json","/cfgfolder/qbittorrentstats/config.json"]
+    @assert any(isfiletry.(configfiles))
+
+    cfgs,configfile,configfilehash,influxdbsettings = get_config(configfiles)
+    #cfgs,configfile,configfilehash,influxdbsettings = rescan_config(cfgs,configfile,configfilehash,influxdbsettings)
+    #i=2
     
-    sleep(nsecsleep)
+    nsecsleep = 10*60
+    while true
+
+        for i=1:size(cfgs,1)
+            try
+                cfg = cfgs[i]
+                monitor_instance(cfg)
+                cfgs,configfile,configfilehash,influxdbsettings = rescan_config(cfgs,configfile,configfilehash,influxdbsettings);
+            catch er
+                @show er
+            end
+        end
+        
+        sleep(nsecsleep)
+    end
+
+    return nothing 
 end
 
+main_fn()
